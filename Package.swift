aae989c9e79ce654b825937dd1251f7c1227867d
Jason Linscombe <jasonlinscombe@gmail.com>
2020-04-19 16:28:25 +0200

    Initial commit

--- Package.swift
+++ Package.swift
@@ -21,8 +21,5 @@ let package = Package(
         .target(
             name: "DesignableX",
             dependencies: []),
-        .testTarget(
-            name: "DesignableXTests",
-            dependencies: ["DesignableX"]),
     ]
 )
--- Tests/LinuxMain.swift
+++
@@ -1,7 +0,0 @@
-import XCTest
-
-import DesignableXTests
-
-var tests = [XCTestCaseEntry]()
-tests += DesignableXTests.allTests()
-XCTMain(tests)
